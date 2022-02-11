30c6b94917bb5ab78dbf8f7815199fe1d6eaa265
AnonymousWalker <hoanganhtran1998@gmail.com>
2022-02-11 11:48:28 -0600

    clean up code

--- src/main/kotlin/org/bibletranslationtools/app/main/ui/RootView.kt
+++ src/main/kotlin/org/bibletranslationtools/app/main/ui/RootView.kt
@@ -3,35 +3,12 @@ package org.bibletranslationtools.app.main.ui
 import tornadofx.*
 
 class RootView : View() {
-
-    private val navigator: Navigator by inject()
-
     init {
         importStylesheet(javaClass.getResource("/css/my.css").toExternalForm())
         workspace.header.removeFromParent()
     }
 
     override val root = vbox {
-        add(navigator.breadCrumbsBar)
-        hbox {
-            spacing = 10.0
-            paddingAll = 10.0
-
-            button("Back") {
-                setOnAction {
-                    navigator.back()
-                }
-            }
-            button("Forward") {
-                setOnAction {
-                    navigator.forward()
-                }
-            }
-        }
-        add(workspace)
-    }
-
-    override fun onDock() {
-        navigator.dock<ProjectView>()
+        label("hello")
     }
 }
