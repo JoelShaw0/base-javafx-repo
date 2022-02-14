5bc630a3b7a3092f3a31f12e4b0e7656d229424f
AnonymousWalker <hoanganhtran1998@gmail.com>
2022-02-14 10:57:47 -0600

    test

--- src/main/kotlin/org/bibletranslationtools/app/main/ui/RootView.kt
+++ src/main/kotlin/org/bibletranslationtools/app/main/ui/RootView.kt
@@ -8,7 +8,25 @@ class RootView : View() {
         workspace.header.removeFromParent()
     }
 
+    val list = listOf<Int>(1,2,3).toObservable()
+    var i = 4
+
     override val root = vbox {
         label("hello")
+
+        listview(list) {
+
+        }
+
+//        list.onChange { changed ->
+//            changed.next()
+//            println(changed.list)
+//        }
+
+        button("Change List") {
+            setOnAction {
+                list.addAll(i++)
+            }
+        }
     }
 }
