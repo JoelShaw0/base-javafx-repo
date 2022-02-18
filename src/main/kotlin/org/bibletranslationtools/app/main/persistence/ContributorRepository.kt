ce074e7b7a73469f907729d9be12ed3ba878a8e6
anonymouswalker <hoanganhtran1998@gmail.com>
2022-02-18 07:27:25 -0600

    add backend

---
+++ src/main/kotlin/org/bibletranslationtools/app/main/persistence/ContributorRepository.kt
@@ -0,0 +1,14 @@
+package org.bibletranslationtools.app.main.persistence
+
+import org.bibletranslationtools.app.main.entity.Contributor
+
+class ContributorRepository {
+
+    fun getAll(): List<Contributor> {
+        return listOf()
+    }
+
+    fun add(contributor: Contributor) {
+
+    }
+}
